100873a76a2414d791f590f68da924aa342fda98
phoenixwilliams16 <phoenixwilliams16@googlemail.com>
2020-07-02 17:35:39 +0100

    Commit cyntaf pythonCymraeg.py

---
+++ pythonCymraeg.py
@@ -0,0 +1,18 @@
+def allbwn(text):
+    print(text)
+
+
+def add(a, b):
+    return a + b
+
+
+def quadratic(a,b,c,x):
+    return a*pow(x,2)+b*x+c
+
+def annibyniaeth(term):
+    string = [];
+
+    for i in range(len(term)):
+        string[i] = term[i]
+
+    return string
